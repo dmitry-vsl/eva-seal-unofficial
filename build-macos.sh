c18daed9bf32093c37912714e3c3e1533612b83d
Dmitry Vasilev <dmitry.vasil@gmail.com>
2026-04-02 23:38:40 +0700

    fix osx build

--- build-macos.sh
+++ build-macos.sh
@@ -48,6 +48,7 @@ for pyver in 3.10 3.11 3.12 3.13 3.14; do
   cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DUSE_GALOIS=ON \
         -DLLVM_DIR="$(brew --prefix llvm)/lib/cmake/llvm" \
         -DPython_EXECUTABLE="$PYTHON_BIN" \
+        -DPYTHON_EXECUTABLE="$PYTHON_BIN" \  # needed separately for pybind11's FindPythonLibsNew
         -B build .
   cmake --build build -j"$NPROC"
 
